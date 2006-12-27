d161ffa8b8498e9d0924f7f71e98404a43c9d1c2
ondrej.certik <ondrej.certik@2840a766-1120-0410-a7bc-574c379ba612>
2006-12-27 22:27:40 +0000

    stsci settings

--- trunk/calc/halomcmc/mcmc.py
+++ trunk/calc/halomcmc/mcmc.py
@@ -1,8 +1,8 @@
 #import cgitb; cgitb.enable(format="text")
 
 import sys
-#sys.path.append("/data/goods151/ondrej/tests/chemev/utils")
-sys.path.append("/home/ondra/stsci/chemev/utils")
+sys.path.append("/data/goods151/ondrej/tests/chemev/utils")
+#sys.path.append("/home/ondra/stsci/chemev/utils")
 import pickle
 
 import numpy as numarray
