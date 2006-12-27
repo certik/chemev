d7a7b1731ab4cf1dda38b56765b0bcd954500b68
ondrej.certik <ondrej.certik@2840a766-1120-0410-a7bc-574c379ba612>
2006-12-27 22:01:53 +0000

    simulation+isochrones code

---
+++ calc/halomcmc/mcmc.py
@@ -0,0 +1,45 @@
+#import cgitb; cgitb.enable(format="text")
+
+import sys
+sys.path.append("/data/goods151/ondrej/tests/chemev/utils")
+import pickle
+
+import numarray
+
+import iso
+import utils
+import optimization
+
+print "reading isochrones"
+#isodir="m31iso"
+#isodir="m117halo"
+isodir="../../isochrones/117/halo"
+data=iso.readfits(isodir+"/datarr.fits")
+isos = iso.readisos(isodir)
+
+print "fitting"
+pars=eval(file("bestfit117").readline())
+
+ndata=sum(data.flat)
+d = numarray.maximum(data,1e-20)
+llhC=sum( (d*numarray.log(d)).flat )
+iter=0
+out=file("mcmc.out","w")
+def f(params):
+    global iter
+    m=iso.computeCMD(params,isos)
+    C=ndata/sum(m.flat)
+    x=numarray.array(params)
+#    out.write(str(list(x*C))+"\n")
+    pickle.dump(x*C,out,protocol=-1)
+    value=utils.loglikelihood(m*C,data)
+#    out.write("henry: %r tom: %r iter: %r norm: %r\n" 
+#        %(value,2.0*(value+llhC),iter,C))
+    pickle.dump((value,2.0*(value+llhC),iter,C),out,protocol=-1)
+    #out.flush()
+    iter+=1
+    return value
+
+print optimization.minc(optimization.fmin_mcmc,f,pars)
+
+print "done"
