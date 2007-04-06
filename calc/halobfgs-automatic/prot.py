2b7dd5d4f6041ff289c5c0976aaa5140c1738f22
ondrej.certik <ondrej.certik@2840a766-1120-0410-a7bc-574c379ba612>
2007-04-06 19:09:31 +0000

    graph updated

--- calc/halobfgs-automatic/prot.py
+++ calc/halobfgs-automatic/prot.py
@@ -10,20 +10,27 @@ isos = iso.readisos(isodir+"/117/halo")
 
 isow = eval(open("bestfit117").readline())
 model = iso.computeCMD(isow,isos)
-model=utils.normalize(model,sum(data.flat))
+model = utils.normalize(model,sum(data.flat))
 
 def plot_residuals(d,m,aspect=0.2):
+    """
+    todo: the same color range for both data and model. the rediduals are ok
+    """
     import pylab
+    pylab.figure()
     pylab.subplot(221)
     pylab.imshow(d,origin='lower',interpolation="nearest",aspect=aspect)
     pylab.title("data")
+    pylab.colorbar()
     pylab.subplot(222)
     pylab.imshow(m,origin='lower',interpolation="nearest",aspect=aspect)
     pylab.title("model")
+    pylab.colorbar()
     pylab.subplot(223)
     residuals = d-m
     pylab.imshow(residuals,origin='lower',interpolation="nearest",aspect=aspect)
     pylab.title("data-model")
+    pylab.colorbar()
     pylab.subplot(224)
     residuals = d-m
     residuals /= m
@@ -31,10 +38,24 @@ def plot_residuals(d,m,aspect=0.2):
     pylab.title("(data-model)/model")
     #pylab.cool()
     #pylab.savefig("graph.eps")
+    pylab.colorbar()
     pylab.savefig("graph.png")
+    #pylab.show()
+
+def plot_weights(w,isos,aspect=0.4):
+    graph=iso.computefehage(w,isos)
+    import pylab
+    pylab.figure()
+    pylab.imshow(graph,origin='lower',interpolation="nearest",aspect=aspect)
+    pylab.title("isochrones weights")
+    #pylab.cool()
+    #pylab.savefig("graph.eps")
+    pylab.colorbar()
+    pylab.savefig("graph-weights.png")
     #pylab.show()
 
 
 print "likelihood: %r, toms: %r\n"%(utils.loglikelihood(model,data),
     utils.tomslikelihood(model,data))
 plot_residuals(data,model)
+plot_weights(isow,isos)
