1e558e15612fe549d547a7187a89b39043c57653
ondrej.certik <ondrej.certik@2840a766-1120-0410-a7bc-574c379ba612>
2007-04-07 16:10:07 +0000

    696 fitting added

---
+++ calc/halobfgs696/117isofit.py
@@ -0,0 +1,45 @@
+import sys
+
+#import numarray
+
+import chemev
+from chemev import iso,optimization,utils
+
+from numpy import array, maximum, log
+
+import time
+
+def fitting_iteration(pars,iter=10):
+    bestfit = [None]
+    ndata=sum(data.flat)
+    def f(params):
+        m=utils.normalize(iso.computeCMD(params,isos),ndata)
+        return utils.loglikelihood(m,data)
+
+    d = maximum(data,1e-20)
+    llhC=sum( (d*log(d)).flat )
+    def b(params,value,iter):
+        "Print status everytime a best fit is found."
+        m=iso.computeCMD(params,isos)
+        C=sum(data.flat)/sum(m.flat)
+        x=array(params)
+        bestfit[0] = x*C
+        file("bestfit117","w").write(str(list(bestfit[0])))
+        print "henry:",value,"tom:",2.0*(value+llhC),"iter:",iter,"norm:",C
+
+    a= optimization.minc(optimization.fmin_bfgs,f,pars,callback=b,iter=iter)
+    b(a, f(a), -1)
+    return a
+
+
+print "reading isochrones"
+data=iso.readfits(chemev.isodir+"/696/halo/datarr.fits")
+isos = iso.readisos(chemev.isodir+"/696/halo")
+
+pars=[1.0]*696
+
+print "fitting"
+
+pars=fitting_iteration(pars,200)
+
+print "done"
